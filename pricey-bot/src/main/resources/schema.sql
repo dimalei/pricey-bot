20746d9fa23016b6509370ba2f1debec82e44adb
dimalei <dima.leigr@gmail.com>
2025-06-28 13:19:38 +0200

    sql fix as proposed by yt comment

--- pricey-bot/src/main/resources/schema.sql
+++ pricey-bot/src/main/resources/schema.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS jobs (
-    id IDENTITY PRIMARY KEY,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     title VARCHAR(255),
     url VARCHAR(255),
     target_property VARCHAR(255)
 );
 CREATE TABLE IF NOT EXISTS intermediate_actions (
-    id IDENTITY PRIMARY KEY,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     job_id BIGINT,
     position INT,
     instruction VARCHAR(255),
